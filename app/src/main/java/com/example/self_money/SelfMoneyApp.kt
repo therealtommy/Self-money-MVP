32f4296fcb0d352c10518da4445484c0e251f1ef
therealtommy <sokolovai1887@ya.ru>
2026-05-15 09:46:07 +0700

    Add custom Application class and repository stub"

---
+++ app/src/main/java/com/example/self_money/SelfMoneyApp.kt
@@ -0,0 +1,9 @@
+package com.example.self_money
+
+import android.app.Application
+
+class SelfMoneyApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/com/example/self_money/repository/FinanceRepository.kt
@@ -0,0 +1,4 @@
+package com.example.self_money.repository
+
+class FinanceRepository {
+}
